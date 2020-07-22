e934f50835271ab6f31cce8e5bb0dfd1ccb20483
luisamaral <la@deutschefintechsolutions.de>
2020-07-22 13:32:11 +0200

    adding styles

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const shopRoutes = require("./routes/shop");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/", (req, res, next) => {
   console.log("This always runs!");
   next();
