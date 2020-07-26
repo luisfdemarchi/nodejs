50177ffbf9b0db8ea59e469120cc27a8b1c6873a
luisamaral <la@deutschefintechsolutions.de>
2020-07-26 15:48:38 +0200

    adding pug templates

--- routes/shop.js
+++ routes/shop.js
@@ -3,8 +3,11 @@ const path = require("path");
 
 const shopRoutes = express.Router();
 
+const adminData = require("./admin");
+
 shopRoutes.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", { products, pageTitle: "Shop", path: "/shop" });
 });
 
 module.exports = shopRoutes;
